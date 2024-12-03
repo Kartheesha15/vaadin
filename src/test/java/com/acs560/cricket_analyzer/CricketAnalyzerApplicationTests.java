7c1946fe485272b51a2d79fb21b4f23fdc52329a
kartheesha <kartheesha15@gmail.com>
2024-12-03 18:58:06 -0500

    Vaadin updated

---
+++ src/test/java/com/acs560/cricket_analyzer/CricketAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acs560.cricket_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO country (id, team) VALUES (11, 'Bangladesh');
+INSERT INTO country (id, team) VALUES (1, 'India');
+INSERT INTO country (id, team) VALUES (2, 'Nepal');
+INSERT INTO country (id, team) VALUES (12, 'New Zealand');
+
+
+INSERT INTO playersdata (name, country_id, matches, innings, notouts, runs, average, strikerate) VALUES ('SA Yadav', 11, 8, 8, 1, 199, 28.42, 135.37);
+INSERT INTO playersdata (name, country_id, matches, innings, notouts, runs, average, strikerate) VALUES ('Aaron Jones', 1, 6, 6, 2, 162, 40.5, 135);
+INSERT INTO playersdata (name, country_id, matches, innings, notouts, runs, average, strikerate) VALUES ('Aasif Sheikh', 2, 3, 3, 0, 63, 21.0, 88.73);
